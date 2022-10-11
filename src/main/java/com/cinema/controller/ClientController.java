0ee5d4a10226d0c47e541c916923587c6933c06c
fabianalejandro1209 <fabian.rubiano.mt@usa.edu.co>
2022-10-10 23:17:40 -0500

    cargue

---
+++ src/main/java/com/cinema/controller/ClientController.java
@@ -0,0 +1,44 @@
+
+package com.cinema.controller;
+
+import com.cinema.model.Client;
+import com.cinema.service.ClientService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/Client")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+public class ClientController {
+    @Autowired
+    private ClientService clientService;
+    
+    @GetMapping("/all")
+    public List<Client> getAll(){
+        return clientService.getAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Optional<Client> getClient(@PathVariable("id") int clientId){
+        return clientService.getClient(clientId);        
+    }
+    
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client c){
+        return clientService.save(c);
+    }
+    
+}
